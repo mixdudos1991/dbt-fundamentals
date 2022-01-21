be50781b624ce0e03373f737c27098454218b532
mixdudos@gmail.com <mixdudos@gmail.com>
2022-01-21 15:48:43 +0000

    Refactoring SQL course

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,24 @@
+with
+
+base_orders as (
+
+    select * from {{source('jaffle_shop', 'orders')}}
+
+),
+
+orders as (
+
+      select 
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status,
+        row_number() over (
+            partition by user_id
+            order by order_date, id) as user_order_seq
+
+      from base_orders
+
+    )
+
+select * from orders
